ba9f9dfada803d49d10f272b962af3f42c467b8b
ravindergil <ravindergill196@gmail.com>
2023-12-05 00:03:51 +0100

    Cypress cloud - project updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '9v1mgk',
+  retries: {
+    runMode: 1,
+    },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners here  
       specPattern='cypress/e2e/testcases/*.js'
     },
   },
